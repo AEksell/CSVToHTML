d3e6c89f60c9f293ebae747aa9fc96016e3f3719
AEksel <akselgab@gmail.com>
2023-11-09 18:48:15 +0100

    Minute changes to comments in python
    Changed and removed some comments to be more descriptive and not useless
    
    #6

--- PythonTask/main.py
+++ PythonTask/main.py
@@ -13,7 +13,7 @@ app.geometry("720x480") #Resolution
 app.title("CSV to HTML") 
 
 title = customtkinter.CTkLabel(app, text="CSV TO HTML")  #Text inside the program that says 'CSV TO HTML'
-title.pack() #.pack is required to finish editing an element
+title.pack() #.pack is required to finish editing an element, always remember to pack your element as to not leave it open.
 
 
 #MAIN FUNCTION
@@ -26,19 +26,19 @@ def select_csv():
     CSVData = datafile.to_html(index=False) #Translates the CSV file into HTML *CODE*, (INDEX FALSE MAKES THE INPUT WORK FOR SOME REASON I DONT KNOW)
     
     text_widget = scrolledtext.ScrolledText(app, width=300, height=400) #Widget is a box where text can be put and can have its own scrollbar 
-    text_widget.pack(padx=10, pady=40)
     text_widget.insert(tk.INSERT, CSVData)
     text_widget.configure(state='disabled')
+    text_widget.pack(padx=10, pady=40)
 
-    open_button.destroy() #Gets rid of button to implement new 
+    open_button.destroy()
 
 
 #BUTTONS
 open_button = ttk.Button( #Need to make this AFTER the function to have it be listed in 'command'
-    app, #what file it goes on
-    text='Open a File', #text on the button
-    command=select_csv) #functionality for the button
+    app, #where the button will go (plainly on the program)
+    text='Open a File',
+    command=select_csv) #Selects function to run
 open_button.pack(expand=True)
 
 
-app.mainloop() #Opens the app (Program)
+app.mainloop() #Properly opens the app (Program)
