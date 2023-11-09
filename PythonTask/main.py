d3bcc4a50a3c40ca0c8f78094699ac5af4d1e307
AEksel <akselgab@gmail.com>
2023-11-09 18:47:31 +0100

    Changed comments
    Changed useless comments and made it more insightful
    
    #6

--- PythonTask/main.py
+++ PythonTask/main.py
@@ -19,16 +19,16 @@ title.pack() #.pack is required to finish editing an element
 #MAIN FUNCTION
 def select_csv():
 
-    #SELECTS THE CSV FILE
+    #SELECTS THE CSV FILE                                        Only allows CSV files, might change to allow EXCL too if i can figure it out
     csv = fd.askopenfilename(title = "Select file",filetypes = (("CSV Files","*.csv"),))
 
-    datafile = pd.read_csv(csv) #Reads CSV file
+    datafile = pd.read_csv(csv)
     CSVData = datafile.to_html(index=False) #Translates the CSV file into HTML *CODE*, (INDEX FALSE MAKES THE INPUT WORK FOR SOME REASON I DONT KNOW)
     
-    text_widget = scrolledtext.ScrolledText(app, width=300, height=400) #Adds the widget to put in the html GUI 
-    text_widget.pack(padx=10, pady=40) #packs it up and adds padding for space
-    text_widget.insert(tk.INSERT, CSVData) #inserts the HTML
-    text_widget.configure(state='disabled') # Makes it so you cant change the HTML code
+    text_widget = scrolledtext.ScrolledText(app, width=300, height=400) #Widget is a box where text can be put and can have its own scrollbar 
+    text_widget.pack(padx=10, pady=40)
+    text_widget.insert(tk.INSERT, CSVData)
+    text_widget.configure(state='disabled')
 
     open_button.destroy() #Gets rid of button to implement new 
 
